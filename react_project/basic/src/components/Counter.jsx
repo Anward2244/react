7bcea0380d5a2f1a69cd0f6767ac8b11c1fc6b49
Anward2244 <danwar2805@gmail.com>
2026-01-03 10:11:22 +0530

    counter styles updated

--- react_project/basic/src/components/Counter.jsx
+++ react_project/basic/src/components/Counter.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import "../ClassComponents/counter.css"
 
 function CounterApp(){
     let [count, setCount] = useState(0)
@@ -8,12 +9,14 @@ function CounterApp(){
     let decrement = ()=>{
         setCount(count-1)
     }
+    let count_color = count>0 ? 'green': count<0 ? 'red':'black' 
     return(
-        <>
-        <button onClick={increment}>increment</button>
-        <p>Count:{count}</p>
+        <div style={{textAlign:'center'}}>
+        <p>count</p>
+        <h1 style={{color:count_color}}>{count}</h1>
         <button onClick={decrement}>decrement</button>
-        </>
+        <button onClick={increment}>increment</button>
+        </div>
     )
 }
 export default CounterApp
